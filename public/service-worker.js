c138b820c3dcfc3b16790bcfd9bc9759495f5d10
ayfor <josh.a.stubbington@gmail.com>
2021-07-28 19:25:04 -0400

    fixed paths for service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,14 @@
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/style.css",
+  "/styles.css",
   "/manifest.webmanifest",
-  "/assets/images/icons/icon-192x192.png",
-  "/assets/images/icons/icon-512x512.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
-const CACHE_NAME = "static-cache";
-const DATA_CACHE_NAME = "data-cache";
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
 self.addEventListener("install", function(evt) {
